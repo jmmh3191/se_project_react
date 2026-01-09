6632e76f2c07b2a7618e17581f3f02daca95c2d6
jmmh3191 <monterojesus911@gmail.com>
2026-01-09 13:36:48 -0800

    close modal styles applied only, matches figma design

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,48 +1,75 @@
+import "./ModalWithForm.css";
+
 function ModalWithForm() {
   return (
     <div className="modal">
-      <form className="modal__form">
+      <div className="modal__content">
         <h2 className="modal__title">New garment</h2>
-        <button type="button" className="modal__close-btn">
-          CLOSE
-        </button>
-        <label htmlFor="name" className="modal__label">
-          Name{" "}
-          <input
-            type="text"
-            className="modal__input"
-            id="name"
-            placeholder="Name"
-          />
-        </label>
-        <label htmlFor="imageUrl" className="modal__label">
-          Image{" "}
-          <input
-            type="url"
-            className="modal__input"
-            id="imageUrl"
-            placeholder="Image URL"
-          />
-        </label>
-        <fieldset className="modal__radio-buttons">
-          <legend className="modal__legend">Select the weather type:</legend>
-          <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+        <button type="button" className="modal__close-btn"></button>
+        <form className="modal__form">
+          <label htmlFor="name" className="modal__label">
+            Name{" "}
+            <input
+              type="text"
+              className="modal__input"
+              id="name"
+              placeholder="Name"
+            />
           </label>
-          <label
-            htmlFor="warm"
-            className="modal__label modal__label_type_radio"
-          >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+          <label htmlFor="imageUrl" className="modal__label">
+            Image{" "}
+            <input
+              type="url"
+              className="modal__input"
+              id="imageUrl"
+              placeholder="Image URL"
+            />
           </label>
-          <label
-            htmlFor="cold"
-            className="modal__label modal__label_type_radio"
-          >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
-          </label>
-        </fieldset>
-      </form>
+          <fieldset className="modal__radio-buttons">
+            <legend className="modal__legend">Select the weather type:</legend>
+            <label
+              htmlFor="hot"
+              className="modal__label modal__label_type_radio"
+            >
+              <input
+                name="weatherType"
+                id="hot"
+                type="radio"
+                className="modal__radio-input"
+                defaultChecked
+              />
+              <span className="modal__radio-text">Hot</span>
+            </label>
+            <label
+              htmlFor="warm"
+              className="modal__label modal__label_type_radio"
+            >
+              <input
+                name="weatherType"
+                id="warm"
+                type="radio"
+                className="modal__radio-input"
+              />
+              <span className="modal__radio-text">Warm</span>
+            </label>
+            <label
+              htmlFor="cold"
+              className="modal__label modal__label_type_radio"
+            >
+              <input
+                name="weatherType"
+                id="cold"
+                type="radio"
+                className="modal__radio-input"
+              />
+              <span className="modal__radio-text">Cold</span>
+            </label>
+          </fieldset>
+          <button type="submit" className="modal__garment-submit">
+            Add garment
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
